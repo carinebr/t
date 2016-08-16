e50a9b2f858baf64e9131b8a875f7c0299ed0df4
carinebr <yehuda_braun@yahoo.com>
2016-08-16 16:21:55 +0300

    makefile

--- t1/src/t1.cpp
+++ t1/src/t1.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 //git   ouvrir un compte non reconnaisable sous git (nouveau mail???)
 //ouvrir un projet 't' avec ce compte 
-//cloner sous ~ ce projet 
+//cloner sous ~ ce projet   clone https://github.com/yehudabr/t
+//     le proj est vide
+//git config user.email "yehuda_braun@yahoo.com"     c local au repertoire/repository
+//git config user.name "<le nom>" idem!!
+//ajouter les fichiers
+//commit push etc...
 //
 //OK transformer Appli en singletone. 
 //makefile
