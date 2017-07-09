410443c246adb1c2036c82739dedc10a98b08f5c
carinebr <yehuda_braun@yahoo.com>
2017-07-09 15:56:03 +0300

    fonction template recevant comme argument un conteneur stl

--- common/inc/arraystmpl.h
+++ common/inc/arraystmpl.h
@@ -5,6 +5,7 @@
  *
  */
 #include <string>
+#include<vector>
 using namespace std;
 //template <typename Type>
 class ArraysTmpl
@@ -22,6 +23,20 @@ T aSimpleTmplFunc(T arg1, T arg2)
 {
     return arg1;
 }
+/**
+ * \fn minimum
+ * \brief renvoie le + petit de 2 element
+ * appel: ex: int a = ., b= ..    minimum<int, int>(a, b);
+ * \param[in] arg1, arg2
+ * \return le + petit
+ */
+template <typename Type1, typename Type2>
+Type2 minimum(Type1 arg1, Type2 arg2)
+{
+    if ((Type2) arg1< arg2)
+        return (Type2)arg1;
+    return arg2;
+}
 
 template <typename Type1, typename Type2>
 Type2 average(Type1 arg[] , int taille)
@@ -31,6 +46,21 @@ Type2 average(Type1 arg[] , int taille)
     for (i = 0; i < taille; i++)
         sum += arg[i];
     Type2 ret = (Type2)sum/(Type2)i;
+    return ret;
+}
+
+
+template <typename Type1, typename Type2>
+Type2 average2(std::vector<Type1> in_vec)
+{
+    Type2 ret;
+    int taille(in_vec.size());
+    typename vector<Type1>::iterator itr;
+    Type1 sum(0);
+
+    for (itr = in_vec.begin(); itr != in_vec.end(); itr++)
+        sum+=*itr;
+    ret = (Type2)sum/(Type2)taille;
     return ret;
 }
 
