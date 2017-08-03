8b64909a3b8c992996363d448d481b00c26b3931
carinebr <yehuda_braun@yahoo.com>
2017-08-03 16:13:56 +0300

    mysql handling basic(tres basic)

---
+++ common/inc/sqlclient.h
@@ -0,0 +1,27 @@
+#ifndef SQLCLIENT_H_INCLUDED
+#define SQLCLIENT_H_INCLUDED
+
+#include <string>
+#include <mysql/mysql.h>
+
+using namespace std;
+
+
+/// \file logger.h
+/// \brief log handler header
+class SqlClient
+{
+    public :
+        ///constructor
+        SqlClient();
+        ///destructor
+        ~SqlClient();
+        ///copy constructor
+        SqlClient(const SqlClient& in_anotherSqlClient);
+    private:
+        MYSQL *conn;
+        MYSQL_RES *res;
+        MYSQL_ROW row;
+        string m_sSqlClient;
+};
+#endif  //SQLCLIENT_H_INCLUDED
---
+++ common/src/sqlclient.cpp
@@ -0,0 +1,26 @@
+/// \file sqlclient.cpp
+/// \brief functionnality testing implementation
+
+#include "sqlclient.h"
+
+
+/// \fn SqlClient::SqlClient(void)
+/// constructor
+/// 
+SqlClient::SqlClient()
+{
+}
+
+/// \fn SqlClient::~SqlClient(void)
+/// destructor
+SqlClient::~SqlClient(void)
+{
+}
+
+/**
+ * \fn SqlClient::SqlClient(const SqlClient& in_anotherSqlClient)
+ * \brief copy construtor
+ */
+SqlClient::SqlClient(const SqlClient& in_anotherSqlClient)
+{
+}
